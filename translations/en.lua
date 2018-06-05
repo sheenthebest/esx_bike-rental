65de97e5ca4ee7602f469de2a856d19b0fbe3dd7
Andy Grenade <33361469+sheenthebest@users.noreply.github.com>
2018-06-05 22:56:37 +0200

    Update en.lua

--- translations/en.lua
+++ translations/en.lua
@@ -7,6 +7,7 @@ Locales['en'] = {
 	['bike'] = 'Bike - <span style="color:green;">TriBike</span> <span style="color:red;">89$</span>',
 	['bike2'] = 'Bike - <span style="color:green;">Scorcher</span> <span style="color:red;">99$</span>',
 	['bike3'] = 'Bike - <span style="color:green;">Cruiser</span> <span style="color:red;">129$</span>',
+	['bike4'] = 'Bike - <span style="color:green;">BMX</span> <span style="color:red;">109$</span>',
 	
 	['bikemessage'] = 'We hope you enjoyed the ride. Come back later. :)',
 	
