1b084ae19d530f4d49c4f180ffc7ac688760e59b
Andy Grenade <33361469+sheenthebest@users.noreply.github.com>
2018-06-05 22:56:22 +0200

    Update cz.lua

--- translations/cz.lua
+++ translations/cz.lua
@@ -7,6 +7,7 @@ Locales['cz'] = {
 	['bike'] = 'Kolo - <span style="color:green;">TriBike</span> <span style="color:red;">89$</span>',
 	['bike2'] = 'Kolo - <span style="color:green;">Scorcher</span> <span style="color:red;">99$</span>',
 	['bike3'] = 'Kolo - <span style="color:green;">Cruiser</span> <span style="color:red;">129$</span>',
+	['bike4'] = 'Kolo - <span style="color:green;">BMX</span> <span style="color:red;">109$</span>',
 	
 	['bikemessage'] = 'Doufáme ,že jsi jízdu užil. Děkujeme přidte zas. :)',
 	['notabike'] = 'Nejsi na kole!',
